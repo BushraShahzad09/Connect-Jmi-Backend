586c182718d75ee311566b74ff8505332777d748
AIN <mdnoor20205@gmail.com>
2023-04-24 20:36:02 +0530

    changes in env file

--- connect.js
+++ connect.js
@@ -1,8 +1,9 @@
 import mysql, { createConnection } from 'mysql'
-
+import * as dotenv from 'dotenv';
+dotenv.config();
 export const db=mysql.createConnection({
     host: 'localhost',
-    user: 'root',
-    password: 'toor',
-    database: 'jmi_connect'
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB
 })
