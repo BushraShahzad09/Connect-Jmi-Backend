a2898deb6e414826c1d09d1512a16f90380519d6
HammadFaizv <hammadfaza12345@gmail.com>
2023-04-25 02:38:43 +0530

    otp verification and resend done

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,13 @@
 import express from "express";
-import { login, register, logout } from "../controllers/auth.js";
+import { login, register, otpVerification, resendOTP, logout } from "../controllers/auth.js";
 import bodyParser from "body-parser";
 const encoder = bodyParser.urlencoded();
 const router=express.Router();
 
 router.post("/login",login)
 router.post("/register",encoder, register)
+router.post("/otpVerification",otpVerification)
+router.post("/resendOTP", resendOTP)
 router.post("/logout",logout)
 
 export default router
