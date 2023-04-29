22fdc62cc4ab3f6e550e83f94c296d0a36b9b0e2
simplyarsa <simplyarsa15@gmail.com>
2023-04-29 16:19:49 +0530

    profile box routes

--- controllers/user.js
+++ controllers/user.js
@@ -12,3 +12,12 @@ export const getUserInfo=(req, res) => {
         return res.status(200).json(data);
     })
 }
+
+export const getPostsNumber=(req, res)=>{
+    const currentUser = req.params.user;
+    db.query("Select (select count(*) from posts where userid=?) as pno, (select count(*) from comments where userid=?) as cno" ,[currentUser, currentUser], (err, data) => {
+        if(err) return res.status(500).json(err);
+        // console.log(data);
+        return res.status(200).json(data);
+    })
+}
